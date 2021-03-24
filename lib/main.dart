40722e66fbc31091ef912e2b9545a594eeb87655
IshanDhruv <ishandhruv22@gmail.com>
2021-03-24 14:43:17 +0530

    Code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'presentation/screens/splash_screen.dart';
 
-void main() {
+main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ProviderScope(child: MyApp()));
 }
 
