8fbb5b121e78e4faccc9f0750735922a6c292ebd
IshanDhruv <ishandhruv22@gmail.com>
2021-03-26 10:56:16 +0530

    Added scan button UI

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -18,7 +18,37 @@ class HomeScreen extends ConsumerWidget {
           )
         ],
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              height: 100,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)),
+                  primary: Colors.green[400],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      'assets/traQR.png',
+                      height: 80,
+                    ),
+                    Text(
+                      "Scan QR Code",
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ],
+                ),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
