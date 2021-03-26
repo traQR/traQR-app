74d04ff6d1c032818094608defa7fb2b170da122
IshanDhruv <ishandhruv22@gmail.com>
2021-03-26 15:54:27 +0530

    created course class

---
+++ lib/models/course.dart
@@ -0,0 +1,14 @@
+class Class {
+  final String title;
+  final double attendance;
+  final String slot;
+  final int attendedClasses;
+  final int missedClasses;
+
+  Class(
+      {this.title,
+      this.attendance,
+      this.slot,
+      this.attendedClasses,
+      this.missedClasses});
+}
