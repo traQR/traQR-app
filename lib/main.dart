f7ee5865d4a0f49681b62b9844cbc32fd51172b7
IshanDhruv <ishandhruv22@gmail.com>
2021-03-27 11:05:35 +0530

    Updated course card and theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.green),
       home: SplashScreen(),
     );
   }
--- lib/presentation/widgets/class_card.dart
+++ lib/presentation/widgets/class_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:traqr_app/models/course.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class ClassCard extends StatelessWidget {
   final Course course;
@@ -15,34 +16,79 @@ class ClassCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         child: Container(
           padding: EdgeInsets.all(15),
-          color: Colors.indigo[100],
-          child: Column(
+          color: Colors.blue[100],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(course.title),
-                  Text(' ${(attendance * 100).round().toString()}% '),
-                ],
-              ),
-              Text(course.slot),
-              RichText(
-                text: TextSpan(
-                  text: course.attendedClasses.toString(),
-                  style: TextStyle(color: Colors.green),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: ' / ',
-                      style: TextStyle(color: Colors.black),
+              Container(
+                width: 250,
+                height: 120,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      course.title,
+                      overflow: TextOverflow.visible,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                    TextSpan(
-                        text: course.missedClasses.toString(),
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.red)),
+                    Text(
+                      course.slot,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            padding: EdgeInsets.all(5),
+                            color: Colors.green,
+                            child: Center(
+                              child: Text(
+                                course.attendedClasses.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            padding: EdgeInsets.all(5),
+                            color: Colors.red[500],
+                            child: Center(
+                              child: Text(
+                                course.missedClasses.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-              )
+              ),
+              CircularPercentIndicator(
+                radius: 100.0,
+                lineWidth: 10.0,
+                percent: attendance,
+                center: Text(' ${(attendance * 100).round().toString()}% '),
+                backgroundColor: Colors.grey,
+                progressColor: attendance > 0.75 ? Colors.green : Colors.red,
+              ),
             ],
           ),
         ),
