b11aba4a2160fd951a0d40c4dd410e7aba2f24cf
IshanDhruv <ishandhruv22@gmail.com>
2021-03-27 11:48:10 +0530

    Added basic scan functionality

--- lib/presentation/screens/home/scan_screen.dart
+++ lib/presentation/screens/home/scan_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanScreen extends StatefulWidget {
   @override
@@ -6,6 +8,40 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> {
+  String _scanBarcode = 'Unknown';
+
+  @override
+  void initState() {
+    startBarcodeScanStream() async {
+      FlutterBarcodeScanner.getBarcodeStreamReceiver(
+              "#ff6666", "Cancel", true, ScanMode.BARCODE)
+          .listen((barcode) => print(barcode));
+    }
+
+    super.initState();
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,9 +69,12 @@ class _ScanScreenState extends State<ScanScreen> {
                   ),
                 ],
               ),
-              onPressed: () {},
+              onPressed: () {
+                scanQR();
+              },
             ),
-          )
+          ),
+          Text('Scan result : $_scanBarcode\n', style: TextStyle(fontSize: 20))
         ],
       ),
     );
