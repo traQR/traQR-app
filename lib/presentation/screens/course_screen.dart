9175a8b7d6850fd8079961216ee2bf06e740922e
IshanDhruv <ishandhruv22@gmail.com>
2021-05-14 17:46:16 +0530

    removed chat screen button

--- lib/presentation/screens/course_screen.dart
+++ lib/presentation/screens/course_screen.dart
@@ -33,19 +33,19 @@ class _CourseScreenState extends State<CourseScreen> {
       appBar: AppBar(
         title: Text("traQR"),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.chat_bubble_rounded,
-          color: grey,
-        ),
-        onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ChatScreen(
-                course: widget.course,
-              ),
-            )),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(
+      //     Icons.chat_bubble_rounded,
+      //     color: grey,
+      //   ),
+      //   onPressed: () => Navigator.push(
+      //       context,
+      //       MaterialPageRoute(
+      //         builder: (context) => ChatScreen(
+      //           course: widget.course,
+      //         ),
+      //       )),
+      // ),
       body: Container(
           child: SingleChildScrollView(
         child: Column(
